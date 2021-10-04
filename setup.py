beabd77df163e9aceb5355c96164c7dca856f51d
dvischi <dvischi@users.noreply.github.com>
2021-10-04 09:54:43 +0200

    Add license/documentation
    * Add license
    * Add documentation
    * Code cleanup

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='fractal-napari-plugins-colletion:ijroi-reader',
-    version='1.0.0',
+    version='1.1.0',
     author='Dario Vischi, Marco Franzon',
     author_email='dario.vischi@fmi.ch, marco.franzon@exact-lab.it',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    license='MIT',
+    license='BSD3',
     description=(
         'Plugin for reading ImageJ ROI files into Napari.'
     ),
     long_description=open('README.md').read(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
-        "napari[all] >= 0.3.8",
+        "napari[all] >= 0.4.7",
         "napari_plugin_engine >= 0.1.9",
-        "dask[complete] >= 2021.3.0",
-        "numpy >= 1.19.5",
-        "imagecodecs >= 2020.5.30"
+        "dask[complete] >= 2021.4.0",
+        "numpy >= 1.20.2",
+        "imagecodecs >= 2020.5.30",
     ],
     entry_points={
         'napari.plugin': [
